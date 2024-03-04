f009293527ef4febb4feeccfbb92e8ba11361c1c
anton-domenikov <home-user-linux@DESKTOP-DQ3AT8I>
2024-03-04 12:09:43 +0200

    typo in staging orders, plus new marts model

--- elt_demo/models/staging/stg_tpch_orders.sql
+++ elt_demo/models/staging/stg_tpch_orders.sql
@@ -3,6 +3,6 @@ select
     o_custkey as customer_key,
     o_orderstatus as status_code,
     o_totalprice as total_price,
-    o_orderdata as order_date
+    o_orderdate as order_date
 from
     {{ source('tpch', 'orders') }}
