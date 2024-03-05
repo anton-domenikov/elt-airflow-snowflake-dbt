0151591f5919a6b82f9d5a69a616ba5fcad41e74
anton-domenikov <anton.domenikov@gmail.com>
2024-03-05 08:04:24 +0200

    added reqs for astro and a dag

---
+++ dags/dbt_dag.py
@@ -0,0 +1,30 @@
+import os
+from datetime import datetime
+
+from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
+from cosmos.profiles import SnowflakeUserPasswordProfileMapping
+
+profile_config = ProfileConfig(
+    profile_name="default",
+    target_name="dev",
+    profile_mapping=SnowflakeUserPasswordProfileMapping(
+        conn_id="snowflake_conn",
+        profile_args={"database": "demo_dbt", "schema": "dbt_schema"},
+    ),
+)
+
+my_cosmos_dag = DbtDag(
+    project_config=ProjectConfig(
+        "/usr/local/airflow/dags/elt_demo",
+    ),
+    operator_args={"install_deps": True},
+    profile_config=profile_config,
+    execution_config=ExecutionConfig(
+        dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",
+    ),
+    # normal dag parameters
+    schedule_interval="@daily",
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    dag_id="dbt_dag"
+)
